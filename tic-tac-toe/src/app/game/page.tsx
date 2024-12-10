7407ade1bab0f1a79fd75efbd9358c1dfc91afdd
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-12-09 16:48:26 -0800

    Added some ui elements

--- tic-tac-toe/src/app/game/page.tsx
+++ tic-tac-toe/src/app/game/page.tsx
@@ -3,27 +3,30 @@ import Image from "next/image"
 
 export default function NumPlayers() {
     return (
-        <div>
-            <main className="grid grid-areas-playersScreen">
-                <Image
+        <div className="flex flex-col justify-center items-center gap-y-24">
+            <h1>Select number of players:</h1>
+            <main className="grid grid-areas-playersScreen gap-4 ">
+                <h1 className="grid-in-imageOne border-4 h-64 w-32 flex justify-center items-center text-9xl">1</h1>
+                {/* <Image
                     className="grid-in-imageOne"
                     src=""
                     alt="One player"
                     width="50"
                     height="50"
-                />
+                /> */}
 
-                <h1 className="grid-in-one">One</h1>
+                <h1 className="grid-in-one text-center">One</h1>
 
-                <Image
+                <h1 className="grid-in-imageTwo border-4 h-64 w-32 flex justify-center items-center text-9xl">2</h1>
+                {/* <Image
                     className="grid-in-imageTwo"
                     src=""
                     alt="Two player"
                     width="50"
                     height="50"
-                />
+                /> */}
 
-                <h1 className="grid-in-two">Two</h1>
+                <h1 className="grid-in-two text-center">Two</h1>
 
             </main>
         </div>
--- tic-tac-toe/src/app/layout.tsx
+++ tic-tac-toe/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <title>Tic Tac Toe</title>
       <body className='grid grid-rows-[auto_1fr_auto] grid-cols-1 w-full min-h-svh'>
         <Header />
         <main className="flex items-center justify-center">{children}</main>
