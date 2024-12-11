33fe6fa1543d040dc4f15004976de76d40c8fa66
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-12-10 22:01:02 -0800

    Set bottom portion of title screen correctly

--- tic-tac-toe/src/app/layout.tsx
+++ tic-tac-toe/src/app/layout.tsx
@@ -8,9 +8,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <title>Tic Tac Toe</title>
         <link rel="icon" href="/tic-tac-toe.ico" />
       </head>
-      <body className='flex flex-col justify-between min-h-svh'>
+      <body className='flex flex-col justify-between w-full min-h-svh'>
         <Header />
-        <main className="flex items-center justify-center">{children}</main>
+        <main className="flex flex-grow items-center justify-center">{children}</main>
       </body>
     </html>
   );
