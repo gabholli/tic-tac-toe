123bca24d9f6b02c2365a63c84269316ae269e64
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-12-10 20:44:46 -0800

    Added confetti when someone wins

--- tic-tac-toe/src/app/components/Board.tsx
+++ tic-tac-toe/src/app/components/Board.tsx
@@ -4,6 +4,8 @@ import calculateWinner from "../utils/calculateWinner"
 import { useEffect, useState } from "react"
 import Square from "../components/Square"
 type Player = "X" | "O" | "BOTH" | null
+import Confetti from "react-confetti"
+
 
 function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null));
@@ -11,10 +13,13 @@ function Board() {
         Math.round(Math.random() * 1) === 1 ? "X" : "O"
     );
     const [winner, setWinner] = useState<Player>(null);
+    const [showConfetti, setShowConfetti] = useState(false)
+
 
     function reset() {
         setSquares(Array(9).fill(null));
         setWinner(null);
+        setShowConfetti(false);
         setCurrentPlayer(Math.round(Math.random() * 1) === 1 ? "X" : "O");
     }
 
@@ -33,6 +38,7 @@ function Board() {
         const w = calculateWinner(squares);
         if (w) {
             setWinner(w);
+            setShowConfetti(true)
         }
 
         if (!w && !squares.filter((square) => !square).length) {
@@ -42,6 +48,7 @@ function Board() {
 
     return (
         <div className="flex flex-col gap-y-4">
+            {showConfetti && <Confetti width={window.innerWidth} height={window.innerHeight} />}
             {!winner && <p className="text-center">Hey {currentPlayer}, it's your turn</p>}
             {winner && winner !== "BOTH" && <p className="text-center">Congratulations {winner}!</p>}
             {winner && winner === "BOTH" && (
@@ -70,3 +77,7 @@ function Board() {
 }
 
 export default Board;
+
+function useWindowSize(): { innerWidth: any; innerHeight: any } {
+    throw new Error("Function not implemented.")
+}
